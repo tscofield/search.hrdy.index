9cac2693512b7fd0cdf118076630098aea4a447d
Thomas Scofield <tscofiel@lxdmts2m99.etrade.com>
2017-11-29 21:44:39 -0500

    first commit

---
+++ bing.py
@@ -0,0 +1,74 @@
+print ('hello, welcome to search.hrdy.index')
+print ('enter the first letter of the latin word below')
+search = input ()
+if search == ('a') or ('A'):    #search a
+    print ('aestate - in summer')
+    print ('altera  - a second')
+    search = input ()
+if search == ('b') or ('B'):    #search b
+    print ('404  No Results')
+    search = input ()
+if search == ('c') or ('C'):    #search C
+    print ('Cur  -  why')
+    search = input ()
+if search == ('d') or ('D '):   #search D
+    print ('dum  -  while')
+    search = input ()
+if search == ('e') or ('E'):   #search E
+    print ('ecce  -  look')
+    print ('et  -  amd')
+    print ('etiam  -  also')
+    search = input ()
+if search == ('f') or ('F'):    #search F
+    print ('404')
+    search = input ()
+if search == ('g') or ('G'):   #search G
+    print ('404')
+    search = input ()
+if search== ('H') or ('h'):    #search H
+    print ('habitat  -  now')
+    search = input ()
+if search== ('i') or ('I'):       #search I
+    print ('iam  -  now')
+    search = input ()
+if search== ('l') or ('L'):    #search L
+    print ('laeta  -  happy')
+    print ('legit  -  reads')
+    search = input ()
+if search== ('m') or ('M'):     #search M
+    print ('404')
+    search = input ()
+if search== ('n') or ('N'):      #searcharkch N
+    print ('nomine  -  name')
+    search = input ()
+if search == ('o') or ('O'):    #search o
+    print ('404')
+    search = input ()
+if search== ('p') or ('P'):      #search P
+    print ('puella  -  girl')
+    search = input ()
+if search== ('q') or ('Q'):      #search Q
+    print ('quae  -  who')
+    print ('quid facit  -  what is doing')
+    print ('quis  -  who')
+    print ('quod  -  because')
+    search = input ()
+if search== ('r') or ('R'):     #search R
+    print ('404')
+    search = input ()
+if search == ('s') or ('S'):    #search S
+    print ('scribit  -  writes')
+    print ('sedet  -  sits')
+    print ('sub arbore  -  under (the) tree')
+    search = input ()
+if search == ('u') or ('U'):    #search U
+    print ('ubi  -  where')
+    search = input ()
+if search == ('v') or ('V'):      #search V
+    print ('vicina  -  neighboring')
+    print ('villa  -  country house')
+    print ('villa  -  rustica')
+    search = input ()
+if search == ('j') or ('k') or ('w') or ('x') or ('y') or ('z'):
+    print ('unavailible')
+    search = input ()  
